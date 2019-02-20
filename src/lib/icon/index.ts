bdf56e89170b3a360ae9c278c53ac3cb8bec01c2
Mahdy Beygi <mahdyb@gmail.com>
2019-02-20 18:27:59 +0330

    fix font awesome

--- src/lib/icon/index.ts
+++ src/lib/icon/index.ts
@@ -1,17 +1,4 @@
 // import all font awesome icons seperately
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faBtc, faEthereum, faGoogle, faGripfire, faTelegramPlane, faTwitter, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
-import { faEye, faEyeSlash, faMoon as Day } from "@fortawesome/free-regular-svg-icons";
-
-import {
-    faAddressCard, faArchive, faArrowLeft, faBalanceScale, faBox, faChartLine, faCheck, faCheckDouble, faCode, faCopy,
-    faDollarSign, faDonate, faEdit, faExchangeAlt,
-    faExternalLinkAlt, faHandHolding, faHandHoldingUsd, faHandshake, faHistory, faHourglassHalf, faKey, faLink, faList,
-    faLock, faMoneyCheckAlt, faMoon, faPlus, faQrcode, faSearch, faShareAlt, faSnowflake, faSquareFull, faThList,
-    faTimes, faUserClock, faWallet, faWifi,
-} from "@fortawesome/free-solid-svg-icons";
-
-library.add(faThList, faCode, faSquareFull, faEye, faShareAlt, faMoon, Day, faEyeSlash, faPlus, faCheckDouble, faWhatsapp,
-    faHandHolding, faArchive, faGoogle, faLock, faUserClock, faLink, faExternalLinkAlt, faList, faTelegramPlane, faTwitter, faWifi,
-    faEdit, faKey, faSearch, faWallet, faHistory, faBtc, faDollarSign, faEthereum, faBox, faBalanceScale, faExchangeAlt, faGripfire, faSnowflake,
-    faDonate, faHandHoldingUsd, faMoneyCheckAlt, faHandshake, faAddressCard, faArrowLeft, faQrcode, faCopy, faChartLine, faCheck, faTimes, faHourglassHalf);
+import { faEye } from "@fortawesome/free-regular-svg-icons/faEye";
+library.add(faEye);
