3794432dd26b106ccc4c2f108a0d0df5270be7dd
Mahdy Beygi <mahdyb@gmail.com>
2019-08-03 17:42:12 +0430

    add skeleton to users page

--- src/containers/Users/index.tsx
+++ src/containers/Users/index.tsx
@@ -1,3 +1,4 @@
+import { Avatar, List, Skeleton } from "antd";
 import axios from "axios";
 import { lstat } from "fs";
 import * as React from "react";
@@ -6,37 +7,56 @@ import Block from "../../components/Holder";
 import t from "../../services/trans/i18n";
 import USER from "./../../lib/user";
 import "./style.less";
-const user = USER.getInstance();
 
 interface IProps {
 }
 
 interface IState {
-    userData: {name: {first: string, last: string} , picture: {large: string}};
+    users: [{name: {first: string, last: string} , picture: {large: string}, email: string }];
 }
 
 class UserContainer extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
-        this.state = {userData : null};
+        this.state = {users : null};
     }
 
     public componentDidMount() {
-        axios.get("https://randomuser.me/api/")
+        axios.get("https://randomuser.me/api?results=4")
         .then((response) => {
           console.log(response.data.results[0]);
-          this.setState({userData : response.data.results[0]});
+          this.setState({users : response.data.results});
         });
     }
 
     public render() {
-        if (this.state.userData === null) { return <div className="users"><Block centerTitle >Loading</Block></div>; }
+        const loading = (this.state.users === null) ? true : false;
+
         return (
-        <div className="users">
-            <Block title={`${this.state.userData.name.first} ${this.state.userData.name.last}`} centerTitle >
-                <img src={this.state.userData.picture.large} alt=""/>
-            </Block>
-        </div> );
+         <div className="users">
+             {
+                 (loading) ?
+                        [...Array(4)].map((n) => <Block><Skeleton loading={loading} active avatar={{size: "large", shape: "square"}} title={false}  paragraph={{rows: 2}}/></Block>) :
+                        <List
+                        itemLayout="horizontal"
+                        dataSource={this.state.users}
+                        renderItem={(user) => (
+                          <Block>
+                          <List.Item>
+                            <List.Item.Meta
+                                avatar={
+                                    <Avatar size="large" shape="square" src={user.picture.large} />
+                                }
+                                title={`${user.name.first} ${user.name.last}`}
+                                description={user.email}
+                            />
+                          </List.Item>
+                          </Block>
+                        )}
+                      />
+             }
+          </div>
+             );
     }
 }
 
